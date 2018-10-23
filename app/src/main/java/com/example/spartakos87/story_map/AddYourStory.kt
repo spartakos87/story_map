5badebb7553e376289dfe381ada15f83e2182415
spartakos87 <serepasf@yahoo.gr>
2018-10-23 20:52:30 +0300

    use SQLlite DB to save and retrieve the url of photo Need many changes to DB class and convert Person to our need , and need to clean code in addyourstory

--- app/src/main/java/com/example/spartakos87/story_map/AddYourStory.kt
+++ app/src/main/java/com/example/spartakos87/story_map/AddYourStory.kt
@@ -8,8 +8,10 @@ import android.os.Bundle
 import android.os.Environment
 import android.os.StrictMode
 import android.provider.MediaStore
+import android.support.design.widget.TextInputEditText
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
+import android.widget.EditText
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_add_your_story.*
@@ -22,7 +24,9 @@ import java.util.*
 
 
 
-@Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+
+
+//@Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
 class AddYourStory : AppCompatActivity() {
 val    storage = FirebaseStorage.getInstance()
 
@@ -30,18 +34,20 @@ val    storage = FirebaseStorage.getInstance()
     private val TAG = "MainActivity"
     var destination: File? = null
     var imagePath: String? = null
-    var photoUrl : Uri? = null
+    var photoUrl : String? = null
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_your_story)
         setSupportActionBar(toolbar)
 
+
         val db: FirebaseFirestore
 
 
         val intent = intent
-        var path = ""
         val lat = intent.getStringExtra("lng")
         val lng = intent.getStringExtra("lng")
         db = FirebaseFirestore.getInstance()
@@ -57,47 +63,85 @@ val    storage = FirebaseStorage.getInstance()
 
             val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(destination));
-            startActivityForResult(intent, REQUEST_IMAGE);
+           startActivityForResult(intent, REQUEST_IMAGE)
 
 
 
         }
+
+        val txtTitle = findViewById<TextInputEditText>(R.id.textInputEditText2)
+        val txtStory = findViewById<EditText>(R.id.editText)
+
+        val btn = findViewById<Button>(R.id.button)
+        btn.setOnClickListener{
+
+            println("????????????????????????>>>>"+photoUrl)
+            var Mydb:MySqlHelper = MySqlHelper(this)
+            println("A=======================>"+Mydb.readAllPerson().toString())
+            val photo_url  = Mydb.readPerson(1.toString())
+            var url =""
+            if (!photo_url.isEmpty()){
+                url =  photo_url.get(0).name.toString()
+                Mydb.deletePerson(1.toString())
+                println("~~~~~~~~~~~~~~~~~~~~~~~~~~>"+url)
+            }
+//            println("B=======================>"+photo_url)
+
+
+            val MyStory: HashMap<String, String> = HashMap<String,String>()
+            MyStory.put("title",txtTitle.text.toString())
+            MyStory.put("story",txtStory.text.toString())
+            MyStory.put("lat",lat)
+            MyStory.put("lng",lng)
+            MyStory.put("url",url)
+            db.collection("Stories").document().set(MyStory as Map<String, Any>)
+            val confirm = Intent(this, MapsActivity::class.java)
+            startActivity(confirm)
+
+        }
+
+
 
 
 
     }
 
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    override  fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == REQUEST_IMAGE && resultCode == Activity.RESULT_OK) {
             try {
                 val `in` = FileInputStream(destination)
                 val options = BitmapFactory.Options()
                 options.inSampleSize = 10
                 imagePath = destination!!.getAbsolutePath()
-                println("~~~~~~~~~~~~~~~~~~~~~~~>"+imagePath)
                 val storageRef = storage.reference
                 val stream = FileInputStream(File(imagePath))
                 val picRef = storageRef.child(dateToString(Date(), "yyyy-MM-dd-hh-mm-ss"))
 
                val  uploadTask = picRef.putStream(stream)
                 uploadTask.addOnFailureListener { exception ->
-                    // Handle unsuccessful uploads
-                    println("@@@@@@@@@@@@@@@@@@@>NOPE")
+
+                    println("Failed")
                 }.addOnSuccessListener { taskSnapshot ->
-                    // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
-                    // ...
-                    println("&&&&&&&&&&&&&&&&&&&> OK")
+
+                    println("OK")
                     picRef.downloadUrl.addOnCompleteListener () {taskSnapshot ->
 
-//                        var url = taskSnapshot.result
-                        photoUrl = taskSnapshot.result
-//                        println ("url =" + url.toString ())
+
+
+                        photoUrl = taskSnapshot.result.toString()
+
+
+                        var db:MySqlHelper = MySqlHelper(this)
+
+                        val person = Person(1, photoUrl.toString(), "F", 38)
+                        db.insert(person)
+
+                        println ("url =" + photoUrl.toString ())
 
                     }
                 }
-//                val file = File(imagePath)
-//                println("++++++++++++++++++++++++++++++>"+file.exists().toString())
+
 
                 val bmp = BitmapFactory.decodeStream(`in`, null, options)
 
@@ -116,6 +160,9 @@ println("Cancel")
         val df = SimpleDateFormat(format)
         return df.format(date)
     }
+
+
+
 
     }
 
---
+++ app/src/main/java/com/example/spartakos87/story_map/Person.kt
@@ -0,0 +1,4 @@
+package com.example.spartakos87.story_map
+
+//data class Person (val id:Int,val name:String,val surname:String,val age:Int)
+data class Person (val id:Int,val name:String,val surname:String,val age:Int)
