9b0338f9bc8797fa60604688b07b6a7e12f679c6
spartakos87 <serepasf@yahoo.gr>
2019-01-16 21:51:07 +0200

    in summary report calculate top3 and winner and start to caclulate all the reports(I didnt run in my phone yet)

--- app/src/main/java/com/example/spartakos87/story_map/PersonalReport.kt
+++ app/src/main/java/com/example/spartakos87/story_map/PersonalReport.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.support.annotation.RequiresApi
 import android.widget.TextView
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import lecho.lib.hellocharts.view.PieChartView
 import lecho.lib.hellocharts.model.SliceValue
@@ -18,29 +19,34 @@ import java.time.format.DateTimeFormatter
 
 class PersonalReport : AppCompatActivity() {
     var username: String = ""
+    val typeReports = AddYourStory().list_of_choices
+    val totalTypeReports = arrayOf("totalΦωτισμός","totalΑπορρήματα","totalΑλλο")
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_personal_report)
 
         val intent = intent
          username = intent.getStringExtra("username")
-        println("SEREPAS current user "+ username)
-        readDB(username)
+        val data = readDB(username)
+        val points = calculaterMonthlyPoints(data,typeReports)
+        val totalPoints = calculaterMonthlyPoints(data,totalTypeReports)
+        val chartPercentans = calculatePieChartShares(data, typeReports)
         val txtUsername = findViewById<TextView>(R.id.textView)
         val txtPointsMonth = findViewById<TextView>(R.id.textView2)
         val txtPointsSum = findViewById<TextView>(R.id.textView5)
         txtUsername.setText("Hello "+username)
-        txtPointsMonth.setText("Points the current month")
-        txtPointsSum.setText("Points summary")
+        txtPointsMonth.setText("Points the current month: "+points)
+        txtPointsSum.setText("Points summary; "+totalPoints)
 
 
 // Add pie chart , use this tutorial https://www.codingdemos.com/android-pie-chart-tutorial
         val pieChartView = findViewById<PieChartView>(R.id.chart)
 val pieValue = ArrayList<SliceValue>()
-        pieValue.add(SliceValue(20f, Color.BLUE))
-        pieValue.add(SliceValue(70f, Color.BLACK))
-        pieValue.add(SliceValue(10f, Color.RED))
+        pieValue.add(SliceValue(chartPercentans.get(typeReports[0])!!.toFloat(), Color.BLUE))
+        pieValue.add(SliceValue(chartPercentans.get(typeReports[1])!!.toFloat(), Color.BLACK))
+        pieValue.add(SliceValue(chartPercentans.get(typeReports[2])!!.toFloat(), Color.RED))
 
 
         val pieChartData = PieChartData(pieValue)
@@ -49,10 +55,10 @@ val pieValue = ArrayList<SliceValue>()
 
     }
 
-    fun calculaterMonthlyPoint(hashReports:HashMap<String,Int>):Int{
+    fun calculaterMonthlyPoints(hashReports:HashMap<String,Int>,typeReports:Array<String>):Int{
 //        Retrive from firebase all reports from the current user
 //        and calculate the current month points of him
-        val typeReports = AddYourStory().list_of_choices
+//        val typeReports = AddYourStory().list_of_choices
         var points:Int = 0
         for (t in typeReports){
             when(t){
@@ -65,10 +71,10 @@ val pieValue = ArrayList<SliceValue>()
     }
 
 
-    fun calculatePieChartShares(hashReports: HashMap<String, Int>):HashMap<String, Double>{
+    fun calculatePieChartShares(hashReports: HashMap<String, Int>,typeReports:Array<String>):HashMap<String, Double>{
         var percentReports:HashMap<String, Double> = HashMap<String, Double>()
         val total = hashReports.get("total")!!.toDouble()
-        val typeReports = AddYourStory().list_of_choices
+//        val typeReports = AddYourStory().list_of_choices
         for (t in typeReports){
             if (total != null && !total.equals(0)) {
                 var temp = hashReports.get(t)!!.toInt().div(total)
@@ -83,12 +89,17 @@ return percentReports
 
 
     @RequiresApi(Build.VERSION_CODES.O)
-    fun readDB(username: String):HashMap<String,Int>{
+    fun readDB(username: String,preMonth:Boolean=false):HashMap<String,Int>{
         //                    Make a map with all reports of current user
         val topicMapper: HashMap<String, Int> = HashMap<String,Int>()
         var fotismos:Int = 0;
         var aporrimata:Int = 0;
         var allo:Int = 0;
+        var totalFotismos:Int = 0;
+        var totalAporrimata:Int = 0;
+        var totalAllo:Int = 0;
+        var tTotal: Int = 0
+
         var total:Int = 0
 
         val db: FirebaseFirestore
@@ -101,6 +112,20 @@ return percentReports
                             if (user != null) {
                                 //First we check if there is the field of username, for legacy reasons mostly
                                 if (document.get("username").toString() == username) {
+                                    // the next step is to check the type
+//                                    in this step calculate the total report for the current user
+
+                                    val type = document.get("type")
+                                    if (type != null) {
+                                        when (document.get("type")) {
+                                            "Φωτισμός" -> totalFotismos += totalFotismos + 1
+                                            "Απορρήματα" -> totalAporrimata += totalAporrimata + 1
+                                            "Αλλο" -> totalAllo += totalAllo + 1
+                                        }
+                                    }
+                                } else {
+//                                    TODO Ignore this data
+
 //                                    Then if this report belong to current user we check the date
                                     val current = LocalDateTime.now()
                                     val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
@@ -109,9 +134,18 @@ return percentReports
                                     val currentYear = formatted.split("-")[0].toInt()
                                     val date = document.get("date")
                                     if (date != null) {
-                                        val saveMonth = date.toString().split("-")[1].toInt()
-                                        val saveYear = date.toString().split("-")[0].toInt()
-
+                                        var saveMonth = date.toString().split("-")[1].toInt()
+                                        var saveYear = date.toString().split("-")[0].toInt()
+// Check the var preMonth. If is false we continue with the current month and year else we calculate
+// for previews month and year
+                                        if (preMonth){
+                                            if (saveMonth-1 == 0){
+                                                saveMonth =12
+                                                saveYear -= 1
+                                            }else{
+                                                saveMonth -= 1
+                                            }
+                                        }
                                         if (currentMonth == saveMonth && currentYear == saveYear) {
 //                                            The report is from current month
 
@@ -136,6 +170,7 @@ return percentReports
                         }}
                         }
                         total = fotismos+aporrimata+allo
+                        tTotal = totalFotismos+totalAporrimata+totalAllo
                         }
 
 
@@ -143,7 +178,11 @@ return percentReports
                   topicMapper.put("Φωτισμός" ,fotismos)
                     topicMapper.put( "Απορρήματα",aporrimata)
                     topicMapper.put("Αλλο",allo)
+                  topicMapper.put("totalΦωτισμός" ,totalFotismos)
+                    topicMapper.put( "totalΑπορρήματα",totalAporrimata)
+                    topicMapper.put("totalΑλλο",totalAllo)
                     topicMapper.put("total",total)
+                    topicMapper.put("tTotal",tTotal)
 
 
 
@@ -152,4 +191,7 @@ topicMapper.put("fail",0)
                 }
        return topicMapper
     }
+
+
+
 }
